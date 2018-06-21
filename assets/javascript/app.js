adbc588301d96b03596c9fd1f01a91544e6a7f3e
Luke Ramsay <lukram98@gmail.com>
2018-06-20 21:21:58 -0400

    moving my changes to my branch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,13 @@
 var API_KEY = '62be46f04f464d4bbc0f89aef3fa9ce4'
 
 $("#submit").on("click", function () {
-    debugger
     event.preventDefault();
     var searchTerm = $("#searchTerm").val();
-    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=" + API_KEY +"&q="+searchTerm
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=" + API_KEY +"&q="+searchTerm;
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
         console.log(JSON.stringify(response))
-    })
-})
-
-
+    });
+});
