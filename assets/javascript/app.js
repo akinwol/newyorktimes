fa418177c231995acf302820ecd93e25f469f977
udugam <ryan.udugam@gmail.com>
2018-06-20 21:34:29 -0400

    added more variables to store values from the form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,13 +3,16 @@ var API_KEY = '62be46f04f464d4bbc0f89aef3fa9ce4'
 $("#submit").on("click", function () {
     event.preventDefault();
     var searchTerm = $("#searchTerm").val();
+    var numResults = $("#inputState").val();
+    var startYear = $("#startYear").val();
+    var endYear = $("#endYear").val();
     var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=" + API_KEY +"&q="+searchTerm
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
-        console.log(response.response.docs.snippet)
-        debugger
+        console.log(response.response.docs)
+        console.log(numResults);
     })
 })
 
